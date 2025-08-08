cfe81d84f1b09cd7c127cd424072badc9d8e46a8
phantomfenix <fenixelectronico@gmail.com>
2025-08-07 23:26:36 -0600

    Update function_proto.py

--- Draft_TestFiles/function_proto.py
+++ Draft_TestFiles/function_proto.py
@@ -1,7 +1,9 @@
 
-get_info = subprocess.run(
+id_ctrl = subprocess.run(
     ["nvme", "id-ctrl", "/dev/nvme0", "-o", "json"],
     capture_output=True,
     text=True
-)  
+)
+id_data = json.loads(id_ctrl.stdout)
+
      serial = id_data.get("sn")
